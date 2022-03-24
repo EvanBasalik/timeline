de4d0999742d8c872c5a700a00827cdcaeb1beb4
EvanBasalik <evanba@microsoft.com>
2022-03-24 18:22:22 -0400

    Fully functional code!

--- timeline.r
+++ timeline.r
@@ -1,15 +1,17 @@
+##original code from https://benalexkeen.com/creating-a-timeline-graphic-using-r-and-ggplot2/
+
 library(ggplot2)
 library(scales)
 library(lubridate)
 
 #read the files
-df <- read.csv('timeline.csv')
+df <- read.csv('timeline-r.csv')
 df
 
 # create a new column with actual date
-#df$date <- with(df, ymd(sprintf('%04d%02d%02d', year, month, 1)))
-#df <- df[with(df, order(date)), ]
-#head(df)
+df$date <- with(df, ymd(sprintf('%04d%02d%02d', year, month, 1)))
+df <- df[with(df, order(date)), ]
+head(df)
 
 #apply colors to the status
 status_levels <- c("Complete", "On Target", "At Risk", "Critical")
@@ -41,13 +43,14 @@ head(df)
 #first part is the months to show
 month_buffer <- 2
 
-month_date_range <- seq(as.Date(min(df$date)) - months(month_buffer), as.Date(max(df$date)) + months(month_buffer), by='month')
+month_date_range <- seq(min(df$date) - months(month_buffer), max(df$date) + months(month_buffer), by='month')
+head(month_date_range)
 month_format <- format(month_date_range, '%b')
 month_df <- data.frame(month_date_range, month_format)
 head(month_df)
 
 #only display years in axis legend that have December/January crossover
-year_date_range <- seq(as.Date(min(df$date)) - months(month_buffer), as.Date(max(df$date)) + months(month_buffer), by='year')
+year_date_range <- seq(min(df$date) - months(month_buffer), max(df$date) + months(month_buffer), by='year')
 year_date_range <- as.Date(
   intersect(
     ceiling_date(year_date_range, unit="year"),
@@ -64,20 +67,19 @@ timeline_plot<-ggplot(df,aes(x=date,y=0, col=status, label=milestone))
 timeline_plot<-timeline_plot+labs(col="Milestones")
 timeline_plot<-timeline_plot+scale_color_manual(values=status_colors, labels=status_levels, drop = FALSE)
 timeline_plot<-timeline_plot+theme_classic()
-print(timeline_plot)
+#print(timeline_plot)
 
 # Plot horizontal black line for timeline
-timeline_plot<-timeline_plot+geom_hline(yintercept=0, 
-                                        color = "black", size=0.3)
-print(timeline_plot)
+timeline_plot<-timeline_plot+geom_hline(yintercept=0,color = "black", size=0.3)
+#print(timeline_plot)
 
 # Plot vertical segment lines for milestones
 timeline_plot<-timeline_plot+geom_segment(data=df[df$month_count == 1,], aes(y=position,yend=0,xend=date), color='black', size=0.2)
-print(timeline_plot)
+#print(timeline_plot)
 
 # Plot scatter points at zero and date
 timeline_plot<-timeline_plot+geom_point(aes(y=0), size=3)
-print(timeline_plot)
+#print(timeline_plot)
 
 # Don't show axes, appropriately position legend
 timeline_plot<-timeline_plot+theme(axis.line.y=element_blank(),
@@ -90,17 +92,17 @@ timeline_plot<-timeline_plot+theme(axis.line.y=element_blank(),
                                    axis.line.x =element_blank(),
                                    legend.position = "bottom"
 )
-print(timeline_plot)
+#print(timeline_plot)
 
 # Show text for each month
-#timeline_plot<-timeline_plot+geom_text(data=month_df, aes(x=month_date_range,y=-0.1,label=month_format),size=2.5,vjust=0.5, color='black', angle=90)
 timeline_plot<-timeline_plot+geom_text(data=month_df, aes(x=month_date_range,y=-0.1,label=month_format),size=2.5,vjust=0.5, color='black', angle=90)
-print(timeline_plot)
+#print(timeline_plot)
 
 # Show year text
 timeline_plot<-timeline_plot+geom_text(data=year_df, aes(x=year_date_range,y=-0.2,label=year_format, fontface="bold"),size=2.5, color='black')
-print(timeline_plot)
+#print(timeline_plot)
 
 # Show text for each milestone
 timeline_plot<-timeline_plot+geom_text(aes(y=text_position,label=milestone),size=2.5)
 print(timeline_plot)
+
